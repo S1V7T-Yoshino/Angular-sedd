15f1ea2a57be7ba18348ec87ef48b064bfea18dd
Yoshino <abdoaymen2@gmail.com>
2022-08-26 00:59:09 +0000

    crud

---
+++ src/app/interface.modal.ts
@@ -0,0 +1,73 @@
+export interface Person {
+    Nom : string,
+    Prenom : string,
+    CIN : string,
+    Tel : string,
+    Adresse : string,
+    uid : string,
+    Num_bureau : string,
+    // salaire : string,
+    image : string,
+}
+export interface Technicien {
+    Nom : string,
+    Prenom : string,
+    CIN : string,
+    Tel : string,
+    Adresse : string,
+    uid : string,
+    matricule : string,
+    date_naissance : string,
+    date_embauche : string,
+    date_depart : string,
+    salaire : string,
+    service : string,
+    grade : string,
+    image : string,
+}
+
+export interface Employee {
+    Nom : string,
+    Prenom : string,
+    CIN : string,
+    Tel : string,
+    Adresse : string,
+    uid : string,
+    Num_bureau : string,
+    // salaire : string,
+    image : string,
+}
+
+export interface helper {
+    Nom : string,
+    Prenom : string,
+    CIN : string,
+    Tel : string,
+    Adresse : string,
+    uid : string,
+    matricule : string,
+    date_naissance : string,
+    date_embauche : string,
+    date_depart : string,
+    salaire : string,
+    service : string,
+    grade : string,
+    image : string,
+}
+
+export interface intervention {
+    Nom : string,
+    Prenom : string,
+    CIN : string,
+    Tel : string,
+    Adresse : string,
+    uid : string,
+    matricule : string,
+    date_naissance : string,
+    date_embauche : string,
+    date_depart : string,
+    salaire : string,
+    service : string,
+    grade : string,
+    image : string,
+}
